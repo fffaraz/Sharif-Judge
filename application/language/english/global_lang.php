7df4c5d9712f1d883e82aae1430b39ba9fe337ae
Faraz Fallahi <fffaraz@gmail.com>
2013-10-27 18:18:46 +0330

    side bar

--- application/language/english/global_lang.php
+++ application/language/english/global_lang.php
@@ -2,5 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$lang['dashboard'] = "Dashboard";
-
+$lang['Dashboard'] = "Dashboard";
+$lang['Settings'] = "Settings";
+$lang['Users'] = "Users";
+$lang['Notifications'] = "Notifications";
+$lang['Assignments'] = "Assignments";
+$lang['Submit'] = "Submit";
+$lang['Final Submissions'] = "Final Submissions";
+$lang['All Submissions'] = "All Submissions";
+$lang['Scoreboard'] = "Scoreboard";
+$lang['Sharif Judge'] = "Sharif Judge";
+$lang['Collapse Menu'] = "Collapse Menu";
+$lang[''] = "";
