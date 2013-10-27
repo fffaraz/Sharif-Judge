df89462b462fb004c215f19c2836c35ee74a21ba
Faraz Fallahi <fffaraz@gmail.com>
2013-10-27 18:37:43 +0330

    change "lang" to "t"

--- application/helpers/language_helper.php
+++ application/helpers/language_helper.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 if ( ! function_exists('lang'))
 {
-	function lang()
+	function t()
 	{
 		$CI =& get_instance();
 		$args = func_get_args();
