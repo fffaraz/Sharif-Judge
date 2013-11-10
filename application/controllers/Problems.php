4393ef30f3e61d6ae1e267218cadd45d483f0357
Faraz Fallahi <fffaraz@gmail.com>
2013-11-10 18:57:18 +0330

    is_participant

--- application/controllers/Problems.php
+++ application/controllers/Problems.php
@@ -43,35 +43,40 @@ class Problems extends CI_Controller
 
 	public function index()
 	{
-		for ($i=0; $i < count($this->problems); $i++) 
-		{ 
-			$this->problems[$i]['question'] = $this->_getQuestion($this->problems[$i]['id']);
-		}
-
-		$data = array(
-			'username' => $this->username,
-			'user_level' => $this->user_level,
-			'all_assignments' => $this->assignment_model->all_assignments(),
-			'all_problems' => $this->problems,
-			'title' => 'Problems',
-			'style' => 'main.css',
-			'success_messages' => $this->success_messages,
-			'error_messages' => $this->error_messages
-		);
-
-		$this->form_validation->set_rules('assignment_select', 'Assignment', 'required|integer|greater_than[0]');
-
-		if ($this->form_validation->run())
+		if ( ! $this->assignment_model->is_participant($this->assignment['participants'], $this->username) )
+			show_error(tr('You are not registered for submitting.'));
+		else
 		{
-			$this->assignment = $this->assignment_model->assignment_info($this->input->post('assignment_select'));
-			$this->user_model->select_assignment($this->username, $this->assignment['id']);
+			for ($i=0; $i < count($this->problems); $i++) 
+			{ 
+				$this->problems[$i]['question'] = $this->_getQuestion($this->problems[$i]['id']);
+			}
+
+			$data = array(
+				'username' => $this->username,
+				'user_level' => $this->user_level,
+				'all_assignments' => $this->assignment_model->all_assignments(),
+				'all_problems' => $this->problems,
+				'title' => 'Problems',
+				'style' => 'main.css',
+				'success_messages' => $this->success_messages,
+				'error_messages' => $this->error_messages
+			);
+
+			$this->form_validation->set_rules('assignment_select', 'Assignment', 'required|integer|greater_than[0]');
+
+			if ($this->form_validation->run())
+			{
+				$this->assignment = $this->assignment_model->assignment_info($this->input->post('assignment_select'));
+				$this->user_model->select_assignment($this->username, $this->assignment['id']);
+			}
+
+			$data['assignment'] = $this->assignment;
+
+			$this->load->view('templates/header', $data);
+			$this->load->view('pages/problems', $data);
+			$this->load->view('templates/footer');
 		}
-
-		$data['assignment'] = $this->assignment;
-
-		$this->load->view('templates/header', $data);
-		$this->load->view('pages/problems', $data);
-		$this->load->view('templates/footer');
 	}
 
 	public function _getQuestion($id)
