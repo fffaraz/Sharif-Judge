7cd0713f345a9a215bedd3735f64d2b8a964fd18
Faraz Fallahi <fffaraz@gmail.com>
2013-12-11 16:50:45 +0330

    counter

--- tester/judge_run.php
+++ tester/judge_run.php
@@ -1,9 +1,13 @@
 <?php
 set_time_limit(0); 
 echo "START\r\n";
+$iiijjj  = 0;
 while (true) 
 {
+	$iiijjj++;
+	echo "Exec $iiijjj \r\n";
 	shell_exec("php queue_process.php");
+	echo "Finish $iiijjj \r\n";
 	sleep(60);
 }
 echo "END\r\n";
